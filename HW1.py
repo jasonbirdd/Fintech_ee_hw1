5315cfae52531dc42ff4b9c7f2f87d142f673d75
jasonbirdd <a51261202@gmail.com>
2021-11-24 22:52:21 +0800

    1124 new

--- HW1.py
+++ HW1.py
@@ -8,8 +8,10 @@ Created on Wed Nov 24 21:19:34 2021
 import os
 import pandas as pd
 
+label = ["date", "time", "price", "volume"]
+TX = pd.DataFrame([], columns=label)
 
 for root, dirs, files in os.walk(".", topdown=False):
     for name in files:
         if name.endswith(".csv"):
-            print(os.path.join(root, name))
+            temp_df = pd.read_csv(os.path.join(root, name), encoding= 'gbk')
